a72f7aa3d4cac648b98e330f1aae2b5b99bfbbc6
Taj88 <taj16@cse.pstu.ac.bd>
2024-09-26 02:03:28 +0600

    Logo

--- src/main-container/top-bar/top-bar.tsx
+++ src/main-container/top-bar/top-bar.tsx
@@ -1,6 +1,6 @@
 import Box from "@mui/material/Box";
-export const TopBarStyles = {
-    border: "1px solid #EBEDF3",
+export const TopBarStyle = {
+    background: "black",
     height: "80px",
     width: "100%", // need to use 1440px
     // backgroundColor: "white",
@@ -9,12 +9,26 @@ export const TopBarStyles = {
     position: "relative", // Keep it relative to ensure scrolling
     margin: 0, // Remove all margins
     boxSizing: "border-box", // Include padding and border in element's width and height
+    padding: "8px 24px 8px 24px", 
 };
-export default function HeaderComponent() {
+export const LogoStyle = {
+  
+    height: "100%",
+    width: "208px", // need to use 1440px
+    backgroundColor: "white",
+    // position: "fixed",
+    display: "flex",
+    position: "relative", // Keep it relative to ensure scrolling
+    margin: 0, // Remove all margins
+    boxSizing: "border-box", // Include padding and border in element's width and height
+    padding: "8px 24px 8px 24px",
+    alignment:"left",
+};
+export default function TopBar() {
     return (
-        <Box sx={HeaderStyles}>
-            <LeftSide />
-            <RightSide />
+        <Box sx={TopBarStyle}>
+           <Box sx={LogoStyle}>
+           </Box>
         </Box>
     );
 }
