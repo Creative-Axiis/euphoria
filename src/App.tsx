e75a7d9ba1cd5a97af3042c1d51a8cad6095b530
Taj88 <taj16@cse.pstu.ac.bd>
2024-11-02 14:04:40 +0600

    refactor: enhance App component and improve EcommerceBody styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import Body from "./main-container/body/body";
 import TopBar from "./main-container/top-bar/top-bar";
 
+/**
+ * Represents the main application component.
+ */
 function App() {
     // Lift the state to App component
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
+    /**
+     * Toggles the visibility of the sidebar.
+     */
     const toggleSidebar = () => {
         setSidebarOpen(!sidebarOpen); // Toggle sidebar visibility
     };
--- src/main-container/body/content-container/E-commerce/body.tsx
+++ src/main-container/body/content-container/E-commerce/body.tsx
@@ -1,33 +1,27 @@
-import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 
 export const EcommerceBodyStyle = {
-    height: "492px",
-    width: "100%", // need to use 1440px
+    height: "492px", // Fixed height
+    minHeight: "492px", // Ensure minimum height is also fixed
+    maxHeight: "492px", // Ensure maximum height is also fixed
     background: "#FAFAFA",
-    // position: "fixed",
     display: "flex",
     position: "relative", // Keep it relative to ensure scrolling
-    // marginLeft: "232px", // Remove all margins
     boxSizing: "border-box", // Include padding and border in element's width and height
     padding: "24px",
     margin: "24px",
-    // overflowY: "auto", // Enable vertical scrolling
-    // zIndex: 999, // Ensure it's above other content
     borderRadius: "16px",
     border: "1px solid #EAEAEA",
     top: 0,
     left: 0,
-
 };
 
+/**
+ * EcommerceBody component renders the main body of the e-commerce section.
+ */
 export default function EcommerceBody() {
-
-
     return (
         <Box sx={EcommerceBodyStyle}>
-
-
         </Box>
     );
 }
